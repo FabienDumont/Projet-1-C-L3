46c3d903d0dbb02d1f2bf13db5d64927d1555749
Enard Charles <charles.enard@gmail.com>
2017-09-27 15:12:47 +0200

    Definitions des fonctions

---
+++ Projet1/Projet1/listchar.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// ================================================
+// Definition des types
+// ================================================
+
+struct cell {
+	char c;
+	struct cell *suiv;
+	struct cell *prec;
+};
+
+typedef struct cell * listchar;
+
+
+// ================================================
+// Definition des operations
+// ================================================
+
+listchar newListe();
+void mvNext(listchar *curs);
+void mvPrev(listchar *curs);
+void gotoFirst(listchar *curs);
+void gotoLast(listchar *curs);
+void addCharNext(listchar *curs);
+void addCharPrev(listchar *curs);
+void delCurrent(listchar *curs);
+void erase(listchar *curs);
+bool isEmpty(listchar curs);
+void printList(listchar curs);
